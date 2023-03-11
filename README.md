be529fecc6bd580879d8fee737769587e9533f10
Matt Sherman <matt@jam123.com>
2023-03-11 09:52:18 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# woocommerce-experimental-enable-new-product-editor
+
+A WordPress plugin that enables the **experimental** new WooCommerce product editor.
+
+## How to use
+
+1. Download latest version of plugin from [releases](https://github.com/Automattic/woocommerce-experimental-enable-new-product-editor/releases).
+2. Install and activate plugin.
+
+## Building a release
+
+1. Manually bump version in `package.json` and `woocommerce-experimental-enable-new-product-editor.php`.
+2. Run...
+
+```text
+npm run plugin-zip
+```
---
+++ woocommerce-experimental-enable-new-product-editor.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Enable Experimental New Product Editor
+ * Plugin URI: https://github.com/Automattic/woocommerce-experimental-enable-new-product-editor
+ * Description: Enables the experimental new WooCommerce product editor.
+ * Author: WooCommerce
+ * Author URI: https://woocommerce.com/
+ * Version: 0.1.0
+ * Requires at least: 5.6
+ * Requires PHP: 7.0
+ *
+ * WC requires at least: 7.5.0
+ * WC tested up to: 7.5.0
+ *
+ * @package WooCommerce\Admin
+ */
+
+function enable_experimental_new_product_editor( $features ) {
+	$features['new-product-management-experience'] = true;
+	return $features;
+}
+
+add_filter( 'woocommerce_admin_get_feature_config', 'enable_experimental_new_product_editor' );
