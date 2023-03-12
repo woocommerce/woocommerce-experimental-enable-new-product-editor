42e94065b53e064f1370539ab5adf52dd2a0611a
Matt Sherman <matt@jam123.com>
2023-03-12 11:02:24 -0400

    Revert "Only enable if not explicitly set"
    This reverts commit 784b5b0f939875134fb2dfcdee77cb4e8e8aff74.

--- woocommerce-experimental-enable-new-product-editor.php
+++ woocommerce-experimental-enable-new-product-editor.php
@@ -5,7 +5,7 @@
  * Description: Enables the experimental new WooCommerce product editor.
  * Author: WooCommerce
  * Author URI: https://woocommerce.com/
- * Version: 0.2.0
+ * Version: 0.1.0
  * Requires at least: 5.6
  * Requires PHP: 7.0
  *
@@ -16,11 +16,7 @@
  */
 
 function enable_experimental_new_product_editor( $features ) {
-	// only enable if it hasn't been explicitly disabled
-	if ( ! isset($features['new-product-management-experience']) ) {
-		$features['new-product-management-experience'] = true;
-	}
-
+	$features['new-product-management-experience'] = true;
 	return $features;
 }
 
